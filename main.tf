66e2fc573b6b7a1d5b446305cf8ea7fa4c8e7de9
Jayesh Agrawat <jayesh.agrawat@NTS-NC-042.local>
2024-01-29 14:58:34 +0530

    assignment

--- main.tf
+++ main.tf
@@ -2,22 +2,24 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "5.13.0"
+      version = "5.6.0"
     }
   }
 }
 
 provider "google" {
-  credentials = file(var.credentials_file)
+  credentials = file(var.credentials)
   project     = var.project
   region      = var.region
 }
+
 
 resource "google_storage_bucket" "demo-bucket" {
   name          = var.gcs_bucket_name
   location      = var.location
   force_destroy = true
-  storage_class = var.gcs_storage_class
+
+
   lifecycle_rule {
     condition {
       age = 1
@@ -28,6 +30,9 @@ resource "google_storage_bucket" "demo-bucket" {
   }
 }
 
-resource "google_bigquery_dataset" "demo-dataset" {
+
+
+resource "google_bigquery_dataset" "demo_dataset" {
   dataset_id = var.bq_dataset_name
+  location   = var.location
 }
--- variables.tf
+++ variables.tf
@@ -1,34 +1,41 @@
-variable "credentials_file" {
-  description = "Path to the credentials file"
+variable "credentials" {
+  description = "My Credentials"
   default     = "./keys/creds.json"
-  
+  #ex: if you have a directory where this file is called keys with your service account json file
+  #saved there as my-creds.json you could use default = "./keys/my-creds.json"
 }
+
+
 variable "project" {
+  description = "Project"
   default     = "first-planet-411908"
-  description = "Project ID"
-}
-variable "location" {
-  default     = "US"
-  description = "Project Location"
 }
 
 variable "region" {
+  description = "Region"
+  #Update the below to your desired region
   default     = "us-central1"
-  description = "Project Region"
+}
+
+variable "location" {
+  description = "Project Location"
+  #Update the below to your desired location
+  default     = "US"
 }
 
 variable "bq_dataset_name" {
-  description = "Bigquery dataset name"
+  description = "My BigQuery Dataset Name"
+  #Update the below to what you want your dataset to be called
   default     = "demo_dataset"
 }
 
-variable "gcs_storage_class" {
-  description = "value of storage class"
-  default     = "STANDARD"
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  #Update the below to a unique bucket name
+  default     = "terraform-demo-terra-bucket-testing111"
 }
 
-variable "gcs_bucket_name" {
-  description = "GCS bucket name"
-  default     = "first-planet-411908-demo-bucket"
-
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
 }
